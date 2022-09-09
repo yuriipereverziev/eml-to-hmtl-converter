0e91c94af1a705f6067f93203ed5551be347103f
yuriipereverziev <yuripereverzev@gmail.com>
2022-09-09 16:33:46 +0300

    test 16

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 80
 app.use(express.static('public/uploads'))
 app.use('/css', express.static(__dirname + '/public/css'))
 app.use('/img', express.static(__dirname + '/public/img'))
+app.use('/js', express.static(__dirname + '/public/js'))
 
 app.use(bodyParser.urlencoded({extended: false}))
-
 
 
 const storage = multer.diskStorage({
@@ -28,14 +28,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage}).single('eml');
 
-
-// app.get('/public', (request, response) => {
-//     response.sendFile(__dirname + '/index.html');
-// })
 app.get('/', function (request, response) {
     response.sendFile(path.join(__dirname + '/public/index.html'));
 });
-
 
 app.post('/convert', (request, response) => {
     upload(request, response, (err) => {

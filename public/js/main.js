1288637a68f00dfaccda07d879df72aaa5e85e36
yuriipereverziev <yuripereverzev@gmail.com>
2022-09-09 16:32:20 +0300

    test 15

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('.form')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    $.ajax({
+        url: "/convert",
+        type: "post",
+        data: new FormData(this),
+        contentType: false,
+        processData: false,
+        success: function success(data) {
+            $('#output').html(data);
+        },
+        error: function error() {
+            console.error("There was an error :(");
+        }
+    });
+})
+
+$('.input-file input[type=file]').on('change', function(){
+    $('.submit-btn').addClass('active')
+});
+
+$('.input-file').click(function() {
+    $('.input-file').removeClass('active')
+});

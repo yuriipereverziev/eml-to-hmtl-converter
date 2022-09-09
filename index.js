70906716bc71808a823a518588ba8399887ef6ec
yuriipereverziev <yuripereverzev@gmail.com>
2022-09-09 13:55:45 +0300

    test

--- index.js
+++ index.js
@@ -11,41 +11,48 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 const PORT = process.env.PORT || 80
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'public/uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
-    }
+
+app.get('/',(req,res) =>{
+    res.end(`
+    <h1>Hello</h1>
+    `)
 })
 
-const upload = multer({storage: storage}).single('eml');
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'public/uploads')
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+//     }
+// })
+//
+// const upload = multer({storage: storage}).single('eml');
+//
+// app.get('/public', (request, response) => {
+//     response.sendFile(__dirname + '/index.html');
+// })
 
-app.get('/public', (request, response) => {
-    response.sendFile(__dirname + '/index.html');
-})
 
-
-app.post('/convert', (request, response) => {
-    upload(request, response, (err) => {
-        if (err) {
-            console.log(err)
-        } else {
-
-            const fileName = path.basename(request.file.filename)
-
-            new EmlParser(fs.createReadStream('public/uploads/' + fileName))
-                .parseEml()
-                .then(result => {
-                    response.send(result.html);
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        }
-    })
-})
+// app.post('/convert', (request, response) => {
+//     upload(request, response, (err) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//
+//             const fileName = path.basename(request.file.filename)
+//
+//             new EmlParser(fs.createReadStream('public/uploads/' + fileName))
+//                 .parseEml()
+//                 .then(result => {
+//                     response.send(result.html);
+//                 })
+//                 .catch(err => {
+//                     console.log(err);
+//                 })
+//         }
+//     })
+// })
 
 
 app.listen(PORT, () => {

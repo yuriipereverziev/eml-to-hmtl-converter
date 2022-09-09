89c69d20c39bc7f9f753ddb3a6b74a4810d39d05
yuriipereverziev <yuripereverzev@gmail.com>
2022-09-09 13:42:30 +0300

    test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage}).single('eml');
 
-app.get('/', (request, response) => {
+app.get('/public', (request, response) => {
     response.sendFile(__dirname + '/index.html');
 })
 
